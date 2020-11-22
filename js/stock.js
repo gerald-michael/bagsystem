66be847ca9c98e74eb8853fa709a1e1df901a5e4
root <tejeca10@gmail.com>
2020-11-23 00:38:42 +0300

    raw header

--- js/stock.js
+++ js/stock.js
@@ -3,3 +3,21 @@ function bing(){
    const cln = el.cloneNode(true);
    document.getElementById('items').appendChild(cln);
 }
+
+
+// products
+document.querySelector('#signup_flow').style.display = 'none';
+
+function view_product(){
+   
+   document.querySelector('#login').onclick = ()=>{
+       document.querySelector('#signup_flow').style.display = 'none';
+       document.querySelector('#login_flow').style.display = 'block';
+       // alert("alive");
+   };
+   document.querySelector('#signup').onclick = ()=>{
+       document.querySelector('#login_flow').style.display = 'none';
+       document.querySelector('#signup_flow').style.display = 'block';
+       
+   };
+}
